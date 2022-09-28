d2e718a6dd94c5fd6bd2c8fb25606fb41454959b
natha.wolff9@gmail.com <>
2022-09-28 16:46:49 -0300

    first commit

---
+++ Wolff_front/wolffbank/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      roxonav: '#bc98ff',
+      roxonavaberta: '#dcbfff',
+      cinzaFundo: '#cbd5e1',
+      
+    }
+  },
+  plugins: [],
+}
